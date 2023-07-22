41712446abca099943e0dc9b3d4204e49e6ef1df
MihikaSingh18 <138354730+MihikaSingh18@users.noreply.github.com>
2023-07-22 17:42:10 +0530

    Add files via upload

---
+++ assignment 5.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      singh
+#
+# Created:     20-07-2023
+# Copyright:   (c) singh 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+
+def my_function(a , b):
+    print(a + " " + b)
+my_function("py" , "scriptor")
+
+
+
+
+
+
+
+def my_function(child1 , child2 , child3):
+    print("The youngest child is" + " " + child3)
+my_function(child1= "ishu" , child2= "puja" , child3= "bulbul")
